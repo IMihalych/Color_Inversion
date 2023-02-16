0c139bb5323d3e5d5daaa07772ebf8b054ebdc71
Petrishchev Ivan <123591030+IMihalych@users.noreply.github.com>
2023-02-16 01:47:27 +0000

    Create mainwindow.h

---
+++ mainwindow.h
@@ -0,0 +1,41 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QFileDialog>
+#include <QThread>
+#include "imageprocessor.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+public slots:
+    void onSelectFile();
+    void onInvertColors();
+
+private slots:
+    void onUpdateImage(QImage image);
+
+private:
+
+    QGraphicsView *graphicsView;
+    ImageProcessor *processor;
+    QString currentFile;
+    QImage image;
+    QGraphicsScene *scene;
+    QThread *thread;
+
+};
+
+#endif // MAINWINDOW_H
